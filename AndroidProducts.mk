72ad8822bbd94b7241629d6fa60cae362d1574c9
Sanju0910 <sreeshankar0910@gmail.com>
2022-11-12 20:37:50 +0530

    avicii: Fixed A13 data decryption & A/B slot switching

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/ofrp_avicii.mk
+
+COMMON_LUNCH_CHOICES := \
+    ofrp_avicii-eng
